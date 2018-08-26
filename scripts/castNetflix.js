d7c2ed08fa2c7dab0dac4bd1117bb6ee3ac25620
Bradley Oakes <BrOakesGames@gmail.com>
2018-08-25 20:18:51 -0400

    fixed dependencies

--- scripts/castNetflix.js
+++ scripts/castNetflix.js
@@ -8,7 +8,7 @@ browser.on('deviceOn', function(device){
     if(device.config.name == 'Ashley and Brad 4evr TV'){
         deviceInUse = device;
     }
-    deviceInUse.play('https://www.netflix.com/watch/70079583?trackId=15035895&tctx=2%2C1%2C304d69e9-41b8-438a-974a-84dbf5ef14f8-67111794%2Ce5c66436-36a2-4dd7-b2d3-5f708812b6c9_75134042X54XX1535237744422%2Ce5c66436-36a2-4dd7-b2d3-5f708812b6c9_ROOT');
+    deviceInUse.play('https://www.hulu.com/watch/1237330');
 
     setTimeout(function () {
         //close the connection
